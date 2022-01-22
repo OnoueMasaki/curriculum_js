39770b3a1fdf422f3fb53f053cde77a08d785b3f
OnoueMasaki <minaseseizon0528@gmail.com>
2022-01-22 19:14:48 +0900

    繰り返し処理
    繰り返し処理の課題の作成

---
+++ JavaScript/1-4/main.js
@@ -0,0 +1,13 @@
+for(let number = 1; number <= 100; number++) {
+    if (number % 3 === 0 && number % 5 === 0) {
+        console.log("FizzBuzz!!");
+        continue;
+    } else if (number % 3 === 0) {
+        console.log("Fizz!");
+        continue;
+    } else if (number % 5 === 0) {
+        console.log("Buzz!");
+        continue;
+    }
+    console.log(number);
+}
